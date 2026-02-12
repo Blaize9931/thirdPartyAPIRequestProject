dad8e2e71125fac28a58fc5151566f754fcac364
Blaize9931 <blaize.kenny@outlook.com>
2026-02-12 19:31:54 +0000

    Adding event listener to Input section & configuring it to display users search

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,19 @@
-fetch('https://pokeapi.co/api/v2/');
+
+
+const searchBar = document.querySelector("input");
+
+searchBar.addEventListener("keydown", (event) => {
+if (event.key == 'Enter') {
+    const userInput = searchBar.value
+    fetch('https://pokeapi.co/api/v2/')
+.then (response => response.json())
+.then (data => console.log(data));
+}
+
+
+});
+
+
+
+
 
