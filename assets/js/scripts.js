b8e925f267abf7a2d03d5efd7e044860fc45fa92
Blaize9931 <blaize.kenny@outlook.com>
2026-02-13 22:30:18 +0000

    end of session.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,15 +4,15 @@ const searchBar = document.querySelector(".input");
 
 
 searchBar.addEventListener("keydown", (event) => {
-if (event.key == 'Enter') {
-    const userInput = searchBar.value
-    fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
-.then (response => response.json())
-.then (data => { console.log(data.abilities)
-pokemonContainer.innerHTML = `<h1>${data.name}</h1> <br> <img src="${data.sprites.front_default}"> <br> <section>
-<p>${userInput}'s first ability is ${data.abilities[0].ability.name}</p>
-<p>${userInput}'s second ability is ${data.abilities[1].ability.name}</p>
-</section>`
-}); 
+    if (event.key == 'Enter') {
+        const userInput = searchBar.value
+        fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
+            .then (response => response.json())
+            .then (data => { console.log(data.abilities)
+            pokemonContainer.innerHTML = `<h1>${data.name}</h1> <br> <img src="${data.sprites.front_default}"> <br> <section>
+            <p>${userInput}'s first ability is ${data.abilities[0].ability.name}</p>
+            <p>${userInput}'s second ability is ${data.abilities[1].ability.name}</p>
+            </section>`
+            }); 
 };
 });
