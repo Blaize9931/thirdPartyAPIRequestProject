5a42c68ec0cfb6c2092bbf02d4e7cb54eccc2342
Blaize9931 <blaize.kenny@outlook.com>
2026-02-13 21:53:24 +0000

    Managed to get sprites to show alongside pokemon name

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,15 @@
 
-const pokemonContainer = querySelector(".container");
+const pokemonContainer = document.querySelector(".container");
 const searchBar = document.querySelector(".input");
 
 
 searchBar.addEventListener("keydown", (event) => {
 if (event.key == 'Enter') {
     const userInput = searchBar.value
-
     fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
 .then (response => response.json())
-.then (data => console.log(data));
-}
-
-pokemonContainer.innerHTML = `<p> ${data.name} </p>`;
+.then (data => { 
+pokemonContainer.innerHTML = `${data.name} <br> <img src="${data.sprites.front_default}">`
+}); 
+};
 });
-
-
-
-
-
-
-
