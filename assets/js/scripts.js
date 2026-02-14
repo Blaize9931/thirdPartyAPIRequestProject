01f7c6530d071d7cf857ecf2071dea774d218e47
Blaize9931 <blaize.kenny@outlook.com>
2026-02-14 13:55:54 +0000

    Added methods to avoid blank entries, and make the input valid for API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@ const searchBar = document.querySelector(".input");
 
 searchBar.addEventListener("keydown", (event) => {
     if (event.key == 'Enter') {
-        const userInput = searchBar.value
+        const userInput = searchBar.value.toLowercase().trim()
         fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
             .then (response => response.json())
             .then (data => { console.log(data.abilities)
@@ -13,6 +13,9 @@ searchBar.addEventListener("keydown", (event) => {
             <p>${userInput}'s first ability is ${data.abilities[0].ability.name}</p>
             <p>${userInput}'s second ability is ${data.abilities[1].ability.name}</p>
             </section>`
-            }); 
-};
+            if (`${userInput}` !== data) {
+                alert("Please provide a valid entry.")
+            }
+            }) 
+        }
 });
