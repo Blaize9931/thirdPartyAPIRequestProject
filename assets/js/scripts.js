9d9f7975c5c520cec1aff6bd05c1fa8671449798
Blaize9931 <blaize.kenny@outlook.com>
2026-02-16 17:43:05 +0000

    configured a function to check if response was okay.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,15 +7,15 @@ searchBar.addEventListener("keydown", (event) => {
     if (event.key == 'Enter') {
         const userInput = searchBar.value.toLowercase().trim()
         fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
-            .then (response => response.json())
+            .then (response => { if (!response.ok) {
+                throw new Error("Network response was not ok")
+            } return response.json()
+        })
             .then (data => { console.log(data.abilities)
             pokemonContainer.innerHTML = `<h1>${data.name}</h1> <br> <img src="${data.sprites.front_default}"> <br> <section>
             <p>${userInput}'s first ability is ${data.abilities[0].ability.name}</p>
             <p>${userInput}'s second ability is ${data.abilities[1].ability.name}</p>
             </section>`
-            if (`${userInput}` !== data) {
-                alert("Please provide a valid entry.")
-            }
             }) 
         }
 });
