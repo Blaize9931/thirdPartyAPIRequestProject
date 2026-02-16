94517230903181e8b50ec6b342b0783160a86250
Blaize9931 <blaize.kenny@outlook.com>
2026-02-16 18:12:34 +0000

    if statement to prevent empty values.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,18 +5,21 @@ const searchBar = document.querySelector(".input");
 
 searchBar.addEventListener("keydown", (event) => {
     if (event.key == 'Enter') {
-        const userInput = searchBar.value.toLowercase().trim()
+        const userInput = searchBar.value.toLowerCase().trim()
+        if (!userInput) {
+            pokemonContainer.innerHTML = ` <p> Please provide a pokemon </p>`;
+            return; 
+        }
         fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
             .then (response => { if (!response.ok) {
                 throw new Error("Network response was not ok")
-            } return response.json()
-        })
+                }; 
+                return response.json(); })
             .then (data => { console.log(data.abilities)
-            pokemonContainer.innerHTML = `<h1>${data.name}</h1> <br> <img src="${data.sprites.front_default}"> <br> <section>
-            <p>${userInput}'s first ability is ${data.abilities[0].ability.name}</p>
-            <p>${userInput}'s second ability is ${data.abilities[1].ability.name}</p>
-            </section>`
-            }) 
-            .catch (error => { console.error("Fetch error", error)
-        }
+                pokemonContainer.innerHTML = `<h1>${data.name}</h1> <br> <img src="${data.sprites.front_default}"> <br> <section>
+                <p>${userInput}'s first ability is ${data.abilities[0].ability.name}</p>
+                <p>${userInput}'s second ability is ${data.abilities[1].ability.name}</p>
+                </section>`; }) 
+            .catch (error => {console.error("Fetch error", error)})
+    }
 });
